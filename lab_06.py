7d8665a15db1af571feaa7d4ddc53b372c058ae4
Popers2004 <140002407+Popers2004@users.noreply.github.com>
2023-07-19 13:21:14 -0400

    Update lab_06.py

--- lab_06.py
+++ lab_06.py
@@ -9,7 +9,23 @@ def encode(password):
 # funtion for the password encoding
     return result
 # return password after encoding
-password = str(input())
-# input in string format of password
-encoded_password = encode(password)
-print(encoded_password)
+
+continue_password = True
+while continue_password:
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    option = int(input('Please enter an option: '))
+    if option == 1:
+        password = str(input('Please enter your password to encode: '))
+        # input in string format of password
+        encoded_password = encode(password)
+        print('Your password has been encoded and stored!')
+    if option == 2:
+    # decoded_password = decode(password)
+        print(f'The encoded password is {encoded_password}, and the original password is {password} ')
+    if option == 3:
+        continue_password = False
+    
