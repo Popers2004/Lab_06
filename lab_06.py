a062ed8c3957d94bbdf3bc11b71f136c273ad73a
Popers2004 <140002407+Popers2004@users.noreply.github.com>
2023-07-19 13:01:10 -0400

    Add files via upload

---
+++ lab_06.py
@@ -0,0 +1,14 @@
+def encode(password):
+# this is the define method for encoding the password
+    result = ""
+# string format of the password
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        result += encoded_digit
+# funtion for the password encoding
+    return result
+# return password after encoding
+password = str(input())
+# input in string format of password
+encoded_password = encode(password)
+print(encoded_password)
